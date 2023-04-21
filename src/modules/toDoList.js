84dc11046d829d6fa8b3edf0c0fdbd06aeb1a5b1
MiloVibes <73610862+MiloVibes@users.noreply.github.com>
2023-04-20 20:19:57 -0500

    reset

--- src/modules/toDoList.js
+++ src/modules/toDoList.js
@@ -4,6 +4,26 @@ class ToDoList {
     this.desc = desc;
     this.comp = comp;
   }
+  
+  toString() {
+    return `Index: ${this.index}, Description: ${this.desc}, Completed: ${this.comp}`;
+  }
+  
+  toggleCompletion() {
+    this.comp = !this.comp;
+  }
+  
+  getCompletionStatus() {
+    return this.comp;
+  }
+  
+  getDescription() {
+    return this.desc;
+  }
+  
+  setDescription(newDesc) {
+    this.desc = newDesc;
+  }
 }
 
 export default ToDoList;
